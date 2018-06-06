a6405d71382646845230470d52f975ce245310b2
chinya07 <kaundanyachinmaya07@gmail.com>
2018-06-06 16:30:19 +0530

    Added CRUD with function based views

--- todo_rest/rest/serializers.py
+++ todo_rest/rest/serializers.py
@@ -5,3 +5,9 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+    def validate_title(self, value):
+        if Task.objects.filter(title=value).exists():
+            raise serializers.ValidationError('TITLE MUST BE UNIQUE!')
+        return title
--- todo_rest/rest/urls.py
+++ todo_rest/rest/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from rest.views import TaskSerializerView
+#from rest.views import TaskSerializerView
+from rest.views import add_task, retrieve_task, delete_task, single_retrieve_task, modify_task
 
-router = routers.DefaultRouter()
-router.register('tasks', TaskSerializerView)
+# router = routers.DefaultRouter()
+# router.register('tasks', TaskSerializerView)
 
 
-urlpatterns = router.urls
+urlpatterns = [ 
+    path('tasks/add/', add_task, name="add_task"),
+    path('tasks/', retrieve_task, name="retrieve_task"),
+    path('tasks/delete/<int:id>/', delete_task, name="delete_task"),
+    path('tasks/modify/<int:id>/', modify_task, name="modify_task"),
+    path('tasks/<int:id>/', single_retrieve_task, name="single_retrieve_task"),
+   
+
+
+]
+
+
